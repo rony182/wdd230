a95819d74fa95b7d06fa516153e8071f63f6d117
rony182 <rony.182@hotmail.com>
2022-01-10 23:18:56 -0300

    done!

--- js/index.js
+++ js/index.js
@@ -27,8 +27,13 @@ let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
 let year = d.getFullYear();
 
+let hour = d.getHours();
+let minutes =d.getMinutes();
+let seconds = d.getSeconds();
 
-let fulldate = dayName + ', ' + monthName + ' ' +d.getDate() + ', ' + year + ', ' + d.getHours();
+
+
+let fulldate = dayName + ', ' + monthName + ' ' +d.getDate() + ', ' + year + ', ' + hour + ':' + minutes + ':' + seconds;
 
 
 document.getElementById('currentdate').textContent = fulldate;
@@ -40,7 +45,10 @@ try {
 		weekday: 'long',
 		day: 'numeric',
 		month: 'short',
-		year: 'numeric'
+		year: 'numeric',
+		hour: 'numeric',
+		minutes: 'numeric',
+		seconds: 'numeric'
 	};
 	document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 } catch (e) {
