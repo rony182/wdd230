0a3772a7a750c3182836088d6bdc00c2516133b5
rony182 <rony.182@hotmail.com>
2022-02-14 23:31:55 -0300

    adding lazy place

---
+++ lesson-7/js/lazy.js
@@ -0,0 +1,32 @@
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const imgOptions= {
+  rootMargin: '0px 0px -100px opx'
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
