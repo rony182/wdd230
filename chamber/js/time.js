4f5cfcb0f26c65751a337cd70ded5ffc4fa60c76
rony182 <rony.182@hotmail.com>
2022-02-17 16:24:02 -0300

    finishing last visit

--- chamber/js/time.js
+++ chamber/js/time.js
@@ -1,6 +1,6 @@
 
 
-let previusVisit= Number(window.localStorage.getItem('visits-ls'));
+// let previusVisit= Number(window.localStorage.getItem('visits-ls'));
 
 // let myStorage=window.localStorage;
 // const lastDate=myStorage.getTime(),
@@ -10,4 +10,20 @@ let previusVisit= Number(window.localStorage.getItem('visits-ls'));
 // const msInDay=1000*60*60*24;
 
 // let difference=Math.round((today-lastDate)/msInDay);
-document.getElementById('time-ago').textContent=previusVisit;
+// document.getElementById('time-ago').textContent=previusVisit;
+
+
+const factor=1000*60*60*24;
+let id=document.getElementById('lastvisit');
+const lastvistit= Number(window.localStorage.getItem('lastvisit'));
+
+let daysbetween= Date.now() - lastvistit
+
+let numberOfDays=Math.round(daysbetween/factor)
+
+if (lastvistit !==0) {
+  id.textContent=`Has been ${numberOfDays} days since the last visit, Welcome Back!`;
+} else{
+  id.textContent='Welcome! This is your first visit';
+}
+localStorage.setItem('lastvisit', Date.now());
