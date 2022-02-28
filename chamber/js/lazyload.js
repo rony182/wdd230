e2206e0ee19aacbe3f72f4bdbddfc59bfce6ab9f
rony182 <rony.182@hotmail.com>
2022-02-28 17:26:00 -0300

    updating html

--- chamber/js/lazyload.js
+++ chamber/js/lazyload.js
@@ -13,7 +13,7 @@ function preloadImage(img) {
 
 const imgOptions= {
   threshold: 1,
-  rootMargin: '0px 0px -10px 0px'
+  rootMargin: '0px 0px -5px 0px'
 };
 
 const imgObserver= new IntersectionObserver((entries, imgObserver) => {
