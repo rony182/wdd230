dd3fdef67eeedacaf3b892dc1ec98abe39d255c7
rony182 <rony.182@hotmail.com>
2022-03-18 23:30:13 -0300

    updating weather

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,10 +1,40 @@
-const temp=parseFloat(document.getElementById('degrees').textContent)
-const wind=parseFloat(document.getElementById('wind-speed').textContent)
+//ADD the key and change units to metric
+const apiURL = '//api.openweathermap.org/data/2.5/weather?id=3838583&appid=de34183783599331759a72cd0a9ba44f&units=metric'
 
-let wc=35.74+(0.6215*temp)-(35.75*Math.pow(wind, 0.16))+(0.4275*temp*Math.pow(wind, 0.16))
-wc=Math.round(wc)
-if (temp <=50 && wind >= 3) {
-   document.getElementById('wind-chill').textContent='Wind Chill is '+wc+'° F';
-} else {
-  document.getElementById('wind-chill').textContent='Not available'
-}
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+    let temperature=weatherInfo.main.temp;
+    temperature=temperature.toFixed(1)
+    let windSpeed=weatherInfo.wind.speed;
+    windSpeed=windSpeed.toFixed(1)
+    
+    document.getElementById('place').innerHTML=weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML=temperature+'° C';
+    document.getElementById('windSpeed').innerHTML='Wind Speed: '+windSpeed;
+
+    const iconsrc= `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`;
+    const desc = weatherInfo.weather[0].description;
+    
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('#weather-desc').textContent = desc+'.';
+    
+    const temp=weatherInfo.main.temp;
+    const wind=weatherInfo.wind.speed;
+
+  let wc=35.74+(0.6215*temp)-(35.75*Math.pow(wind, 0.16))+(0.4275*temp*Math.pow(wind, 0.16))
+  wc=wc.toFixed(1)
+  if (temp <=50 && wind >= 3) {
+    document.getElementById('wind-chill').textContent='Wind Chill: '+wc+'° C';
+  } else {
+    document.getElementById('wind-chill').textContent='Not available'
+  }
+
+
+
+ }); //end of "then" fat arrow function
+
---
+++ lesson-10/js/weatherapi.js
@@ -0,0 +1,16 @@
+const apiURL= 'https://api.openweathermap.org/data/2.5/weather?id=3838583&appid=de34183783599331759a72cd0a9ba44f&units=metric';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  });
+
+  
